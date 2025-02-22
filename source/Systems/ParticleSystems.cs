d1586533c19501501ba19ad74295d0fe1d5d97cd
Phill <philldamaskin@gmail.com>
2025-02-22 16:00:37 -0500

    initial commit

---
+++ source/Systems/ParticleSystems.cs
@@ -0,0 +1,24 @@
+﻿using Simulation;
+using System;
+using Worlds;
+
+namespace Particle.Systems.Systems
+{
+    public readonly partial struct ParticleSystems : ISystem
+    {
+        void ISystem.Start(in SystemContainer systemContainer, in World world)
+        {
+
+        }
+
+        void ISystem.Update(in SystemContainer systemContainer, in World world, in TimeSpan delta)
+        {
+
+        }
+
+        void ISystem.Finish(in SystemContainer systemContainer, in World world)
+        {
+
+        }
+    }
+}
